449abdfb709e8402b1125ee0f4519a74a7bc8b12
dungnv9 <dungnv9@rikkeisoft.com>
2022-05-13 08:55:15 +0700

    update http security config

--- src/main/java/com/dung/demoswagger/configs/WebSecurityConfig.java
+++ src/main/java/com/dung/demoswagger/configs/WebSecurityConfig.java
@@ -1,23 +1,25 @@
 package com.dung.demoswagger.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.endpoint.NimbusAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] AUTH_WHITELIST = {
+
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/v2/api-docs**",
+            "/webjars/**"
+    };
+
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .and()
-                    .authorizeRequests()
-                    .antMatchers("/").permitAll()
-                    .antMatchers("/swagger-ui**").permitAll()
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers(AUTH_WHITELIST).permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .oauth2Login();
